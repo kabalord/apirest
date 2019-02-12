6dd23c7732a8f11c54740ff5292c7922bf115cda
kabalord <wroaserrano@gmail.comgit config --global user.email wroaserrano@gmail.comgit config user.email wroaserrano@gmail.com>
2019-02-12 19:44:41 +0100

    Ajout d'un album

--- src/main/java/com/myaudiolibrary/apirest/controller/AlbumController.java
+++ src/main/java/com/myaudiolibrary/apirest/controller/AlbumController.java
@@ -1,9 +1,30 @@
 package com.myaudiolibrary.apirest.controller;
 
+import com.myaudiolibrary.apirest.model.Album;
+import com.myaudiolibrary.apirest.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/albums")
 
 public class AlbumController {
+
+    @Autowired
+    private AlbumRepository albumRepository;
+
+//  7 - Ajout d'un album
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = "application/json",
+            produces = "application/json"
+    )
+    public Album createAlbum(@RequestBody Album album){
+        return albumRepository.save(album);
+    }
+
 }
--- src/main/java/com/myaudiolibrary/apirest/repository/AlbumRepository.java
+++ src/main/java/com/myaudiolibrary/apirest/repository/AlbumRepository.java
@@ -1,8 +1,11 @@
 package com.myaudiolibrary.apirest.repository;
 
 import com.myaudiolibrary.apirest.model.Album;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+
+public interface AlbumRepository extends PagingAndSortingRepository<Album, Long> {
+
 
 }
